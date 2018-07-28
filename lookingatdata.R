bae63f088c020221288100afa6bc0a4c3f9da8e2
Dee Muni <dipti.muni@gmail.com>
2018-07-28 13:28:41 -0700

    Create lookingatdata.R

---
+++ lookingatdata.R
@@ -0,0 +1,10 @@
+## Credit: grateful to Rswirl and John Hopkins University's course in R programming.
+## Purpose: Domentation of exercises and used as good review and reference. 
+
+#Selection: 12
+
+ |                                                                                 |   0%
+
+| Whenever you're working with a new dataset, the first thing you should do is look at it!
+| What is the format of the data? What are the dimensions? What are the variable names?
+| How are the variables stored? Are there missing data? Are there any flaws in the data?
