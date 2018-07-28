b6961670e5be64c73357535563b9af158a97cbb6
Dee Muni <dipti.muni@gmail.com>
2018-07-28 14:46:42 -0700

    Update lookingatdata.R

--- lookingatdata.R
+++ lookingatdata.R
@@ -208,3 +208,115 @@
 | After previewing the top and bottom of the data, you probably noticed lots of NAs, which
 | are R's placeholders for missing values. Use summary(plants) to get a better feel for
 | how each variable is distributed and how much of the dataset is missing.
+ > summary(plants)
+                     Scientific_Name              Duration   
+ Abelmoschus                 :   1   Perennial        :3031  
+ Abelmoschus esculentus      :   1   Annual           : 682  
+ Abies                       :   1   Annual, Perennial: 179  
+ Abies balsamea              :   1   Annual, Biennial :  95  
+ Abies balsamea var. balsamea:   1   Biennial         :  57  
+ Abutilon                    :   1   (Other)          :  92  
+ (Other)                     :5160   NA's             :1030  
+           Active_Growth_Period      Foliage_Color      pH_Min          pH_Max      
+ Spring and Summer   : 447      Dark Green  :  82   Min.   :3.000   Min.   : 5.100  
+ Spring              : 144      Gray-Green  :  25   1st Qu.:4.500   1st Qu.: 7.000  
+ Spring, Summer, Fall:  95      Green       : 692   Median :5.000   Median : 7.300  
+ Summer              :  92      Red         :   4   Mean   :4.997   Mean   : 7.344  
+ Summer and Fall     :  24      White-Gray  :   9   3rd Qu.:5.500   3rd Qu.: 7.800  
+ (Other)             :  30      Yellow-Green:  20   Max.   :7.000   Max.   :10.000  
+ NA's                :4334      NA's        :4334   NA's   :4327    NA's   :4327    
+   Precip_Min      Precip_Max         Shade_Tolerance   Temp_Min_F    
+ Min.   : 4.00   Min.   : 16.00   Intermediate: 242   Min.   :-79.00  
+ 1st Qu.:16.75   1st Qu.: 55.00   Intolerant  : 349   1st Qu.:-38.00  
+ Median :28.00   Median : 60.00   Tolerant    : 246   Median :-33.00  
+ Mean   :25.57   Mean   : 58.73   NA's        :4329   Mean   :-22.53  
+ 3rd Qu.:32.00   3rd Qu.: 60.00                       3rd Qu.:-18.00  
+ Max.   :60.00   Max.   :200.00                       Max.   : 52.00  
+ NA's   :4338    NA's   :4338                         NA's   :4328    
+
+| That's a job well done!
+
+  |=======================================================                          |  68%
+| summary() provides different output for each variable, depending on its class. For
+| numeric data such as Precip_Min, summary() displays the minimum, 1st quartile, median,
+| mean, 3rd quartile, and maximum. These values help us understand how the data are
+| distributed.
+
+...
+
+  |==========================================================                       |  72%
+| For categorical variables (called 'factor' variables in R), summary() displays the
+| number of times each value (or 'level') occurs in the data. For example, each value of
+| Scientific_Name only appears once, since it is unique to a specific plant. In contrast,
+| the summary for Duration (also a factor variable) tells us that our dataset contains
+| 3031 Perennial plants, 682 Annual plants, etc.
+
+...
+
+  |==============================================================                   |  76%
+| You can see that R truncated the summary for Active_Growth_Period by including a
+| catch-all category called 'Other'. Since it is a categorical/factor variable, we can see
+| how many times each value actually occurs in the data with
+| table(plants$Active_Growth_Period).
+
+> table(plants$Active_Growth_Period)
+
+Fall, Winter and Spring                  Spring         Spring and Fall 
+                     15                     144                      10 
+      Spring and Summer    Spring, Summer, Fall                  Summer 
+                    447                      95                      92 
+        Summer and Fall              Year Round 
+                     24                       5 
+
+| You are amazing!
+
+  |=================================================================                |  80%
+| Each of the functions we've introduced so far has its place in helping you to better
+| understand the structure of your data. However, we've left the best for last....
+
+...
+
+  |====================================================================             |  84%
+| Perhaps the most useful and concise function for understanding the *str*ucture of your
+| data is str(). Give it a try now.
+
+> str(plants)
+'data.frame':	5166 obs. of  10 variables:
+ $ Scientific_Name     : Factor w/ 5166 levels "Abelmoschus",..: 1 2 3 4 5 6 7 8 9 10 ...
+ $ Duration            : Factor w/ 8 levels "Annual","Annual, Biennial",..: NA 4 NA 7 7 NA 1 NA 7 7 ...
+ $ Active_Growth_Period: Factor w/ 8 levels "Fall, Winter and Spring",..: NA NA NA 4 NA NA NA NA 4 NA ...
+ $ Foliage_Color       : Factor w/ 6 levels "Dark Green","Gray-Green",..: NA NA NA 3 NA NA NA NA 3 NA ...
+ $ pH_Min              : num  NA NA NA 4 NA NA NA NA 7 NA ...
+ $ pH_Max              : num  NA NA NA 6 NA NA NA NA 8.5 NA ...
+ $ Precip_Min          : int  NA NA NA 13 NA NA NA NA 4 NA ...
+ $ Precip_Max          : int  NA NA NA 60 NA NA NA NA 20 NA ...
+ $ Shade_Tolerance     : Factor w/ 3 levels "Intermediate",..: NA NA NA 3 NA NA NA NA 2 NA ...
+ $ Temp_Min_F          : int  NA NA NA -43 NA NA NA NA -13 NA ...
+
+| Excellent job!
+
+  |=======================================================================          |  88%
+| The beauty of str() is that it combines many of the features of the other functions
+| you've already seen, all in a concise and readable format. At the very top, it tells us
+| that the class of plants is 'data.frame' and that it has 5166 observations and 10
+| variables. It then gives us the name and class of each variable, as well as a preview of
+| its contents.
+
+...
+
+  |===========================================================================      |  92%
+| str() is actually a very general function that you can use on most objects in R. Any
+| time you want to understand the structure of something (a dataset, function, etc.),
+| str() is a good place to start.
+
+...
+
+  |==============================================================================   |  96%
+| In this lesson, you learned how to get a feel for the structure and contents of a new
+| dataset using a collection of simple and useful functions. Taking the time to do this
+| upfront can save you time and frustration later on in your analysis.
+
+...
+
+  |=================================================================================| 100%
+| Would you like to receive credit for completing this course on Coursera.org?
